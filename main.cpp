00885e7d33d945bd1532b65f4d963f0595256d9e
Denis Shevchenko <cookie-monstr@live.com>
2019-01-03 23:03:29 +0300

    update

--- main.cpp
+++ main.cpp
@@ -3,10 +3,6 @@
 #include "tasks.h"
 
 int main() {
-    Solution solution;
-    std::vector<int> v = {3,3};
-    auto s = solution.twoSum(v,6);
-
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
